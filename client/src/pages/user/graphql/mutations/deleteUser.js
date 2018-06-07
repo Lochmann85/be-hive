fce3470e2a54068f2576032b0f8749482d6078e7
georg lochmann <georg.lochmann@gmail.com>
2018-06-07 20:00:40 +0200

    call delete user and update store

--- client/src/pages/user/graphql/mutations/deleteUser.js
+++ client/src/pages/user/graphql/mutations/deleteUser.js
@@ -1,5 +1,28 @@
 import gql from 'graphql-tag';
 
+import findAllUsersQueryTemplate from '../queries/findAllUsers';
+
+const _deleteUserFromAllUsers = (clientStore, deletedUserId, userFragment) => {
+   const findAllUsersQueryDocument = findAllUsersQueryTemplate(userFragment).document;
+
+   let findAllUsersQuery;
+   try {
+      findAllUsersQuery = clientStore.readQuery({ query: findAllUsersQueryDocument });
+   } catch (error) { }
+
+   if (findAllUsersQuery) {
+      const userIndex = findAllUsersQuery.findAllUsers.findIndex(user => user.id === deletedUserId);
+
+      if (userIndex > -1) {
+         findAllUsersQuery.findAllUsers.splice(userIndex, 1);
+
+         clientStore.writeQuery({
+            query: findAllUsersQueryDocument, data: findAllUsersQuery
+         });
+      }
+   }
+};
+
 export default (userFragment) => ({
    document: gql`
    mutation deleteUser($userId: ID) {
@@ -13,6 +36,9 @@ export default (userFragment) => ({
          deleteUser: (userId) => {
             return mutate({
                variables: { userId },
+               update: (clientStore, { data: { deleteUser } }) => {
+                  _deleteUserFromAllUsers(clientStore, deleteUser.id, userFragment);
+               },
             });
          },
       }),
