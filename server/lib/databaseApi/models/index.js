1ece46be18e848a39818a6603b6203da0ed25df6
georg lochmann <georg.lochmann@gmail.com>
2018-08-12 15:08:36 +0200

    add initial watering station model

--- server/lib/databaseApi/models/index.js
+++ server/lib/databaseApi/models/index.js
@@ -1,5 +1,7 @@
 import * as userModelFactory from './user/userModel';
+import * as wateringStationModelFactory from './wateringStation/wateringStationModel';
 
 export {
    userModelFactory,
+   wateringStationModelFactory,
 };
