f3bc9c2261cfe9eed04959b43dd32c416d0d9a29
georg lochmann <georg.lochmann@gmail.com>
2018-08-18 13:11:49 +0200

    add query for watering stations

--- client/src/pages/wateringStation/WateringStationOverview.jsx
+++ client/src/pages/wateringStation/WateringStationOverview.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-import { Grid } from 'semantic-ui-react';
+import { Grid, Message } from 'semantic-ui-react';
 
 import BaseContentLayout from '../../components/layout/BaseContentLayout';
+import QueryLoader from '../../components/layout/QueryLoader';
 
-const WateringStationOverview = () => {
-   return (
-      <BaseContentLayout title="Watering stations overview" >
+import findAllWateringStationsQuery from './graphql/queries/findAllWateringStations';
+
+const WateringStationOverview = (props) => {
+   const { findAllWateringStationsQuery } = props;
+
+   let content;
+   if (findAllWateringStationsQuery && findAllWateringStationsQuery.findAllWateringStations) {
+      content = (
          <Grid doubling columns={3}>
          </Grid>
+      );
+   }
+   else {
+      content = <Message visible content={"No watering stations found"} />;
+   }
+
+   return (
+      <BaseContentLayout title="Watering stations overview" >
+         <QueryLoader query={findAllWateringStationsQuery}>
+            {content}
+         </QueryLoader>
       </BaseContentLayout>
    );
 };
 
-WateringStationOverview.propTypes = {
+const wateringStationsFragment = {
+   name: "WateringStationsOverview",
+   document: gql`
+   fragment WateringStationsOverview on WateringStation {
+      id
+      name
+      description
+      isActive
+      wateringTimes {
+         duration
+         time
+      }
+   }`
 };
 
 WateringStationOverview.path = (routePath) => `${routePath}/view`;
@@ -23,4 +54,8 @@ WateringStationOverview.menuItem = (routePath) => ({
    path: WateringStationOverview.path(routePath)
 });
 
-export default WateringStationOverview;
+const queryDefinition = findAllWateringStationsQuery(wateringStationsFragment);
+
+export default graphql(queryDefinition.document, queryDefinition.config)(
+   WateringStationOverview
+);
