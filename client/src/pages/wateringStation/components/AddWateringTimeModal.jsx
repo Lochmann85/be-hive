edda5c761af394c78d3dd2a1e39e24ecd3ddc0bf
georg lochmann <georg.lochmann@gmail.com>
2018-08-19 17:21:47 +0200

    add timepicker to add watering time modal

--- client/src/pages/wateringStation/components/AddWateringTimeModal.jsx
+++ client/src/pages/wateringStation/components/AddWateringTimeModal.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TimePicker from 'react-times';
+import 'react-times/css/material/default.css';
 
 import { Modal, Button } from 'semantic-ui-react';
 
 import { BeHiveButton } from '../../../assets/styles/UI';
 
+const defaultTime = {
+   hour: 12,
+   minute: 0,
+   duration: 15
+};
+
 class AddWateringTimeModal extends React.Component {
+
+   constructor(props) {
+      super(props);
+
+      this.state = Object.assign({}, defaultTime);
+   };
 
    render() {
       const {
-         onCloseClick,
-         open
-      } = this.props;
+         hour,
+         minute,
+      } = this.state;
 
       return (
-         <Modal open={open} size="small">
+         <Modal open={this.props.open} size="small">
             <Modal.Header content="Add watering time" />
             <Modal.Content>
+               <TimePicker
+                  onTimeChange={this._handleTimeChange}
+                  time={`${hour}:${minute}`} />
             </Modal.Content>
             <Modal.Actions>
-               <Button content="Close" onClick={onCloseClick} />
+               <Button content="Close" onClick={this._handleCloseClick} />
                <BeHiveButton
                   content="Add"
                   onClick={this._onAddWateringTimeClick} />
@@ -28,8 +45,27 @@ class AddWateringTimeModal extends React.Component {
       );
    }
 
+   _handleTimeChange = (options) => {
+      const {
+         hour,
+         minute
+      } = options;
+
+      this.setState({ hour, minute });
+   };
+
+   _handleCloseClick = () => this.setState(defaultTime, this.props.onCloseClick());
+
    _onAddWateringTimeClick = () => {
-      this.props.onAddWateringTime();
+      const {
+         hour,
+         minute,
+         duration
+      } = this.state;
+
+      this.setState(defaultTime, () => {
+         this.props.onAddWateringTime({ id: 0, time: `${hour}:${minute}`, duration });
+      });
    };
 
 };
